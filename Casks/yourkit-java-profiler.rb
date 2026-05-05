add65b583447b36ee2e6544162e0c2294ad3efcb
Valentin <valentin-nasta@users.noreply.github.com>
2026-05-05 11:21:16 +0200

    Fix URL for YourKit Java Profiler download

--- Casks/yourkit-java-profiler.rb
+++ Casks/yourkit-java-profiler.rb
@@ -5,7 +5,7 @@ cask "yourkit-java-profiler" do
   sha256 arm:   "08a8902a1c8ba1d2f0893f5b13afdd78b7ea5f08c562a1bee904a06f62cbeff9",
          intel: "74b75bf6f0bba8b485cb1080d191b574d5106a15e96c13f49a49c55ddd01cd4f"
 
-  url "https://download.yourkit.com/yjp/#{version}/YourKit-JavaProfiler-#{version}-#{arch}.dmg"
+  url "https://download.yourkit.com/yjp/#{version}/YourKit-Java-Profiler-#{version}-#{arch}.dmg"
   name "YourKit Java Profiler"
   desc "Java CPU and memory profiler"
   homepage "https://www.yourkit.com/features/"
