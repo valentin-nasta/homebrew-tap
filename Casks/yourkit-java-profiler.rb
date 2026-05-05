12ffb59e84904021c52242acf87cfe8e2e64a436
Valentin <valentin-nasta@users.noreply.github.com>
2026-05-05 11:20:01 +0200

    Update YourKit Java Profiler to version 2026.3.157

--- Casks/yourkit-java-profiler.rb
+++ Casks/yourkit-java-profiler.rb
@@ -1,11 +1,11 @@
 cask "yourkit-java-profiler" do
   arch arm: "arm64", intel: "x64"
 
-  version "2025.9,b161"
-  sha256 arm:   "77c901f7c3eba691e637edb34aa81108585e4a894c217ddb236a1ad5cffc3386",
-         intel: "b36cd116732457e77e3d09dc42fe3dfeececaa05aad6bbcc1541fcb1db1d9c28"
+  version "2026.3.157"
+  sha256 arm:   "08a8902a1c8ba1d2f0893f5b13afdd78b7ea5f08c562a1bee904a06f62cbeff9",
+         intel: "74b75bf6f0bba8b485cb1080d191b574d5106a15e96c13f49a49c55ddd01cd4f"
 
-  url "https://download.yourkit.com/yjp/#{version.csv.first}/YourKit-JavaProfiler-#{version.csv.first}-#{version.csv.second}-#{arch}.dmg"
+  url "https://download.yourkit.com/yjp/#{version}/YourKit-JavaProfiler-#{version}-#{arch}.dmg"
   name "YourKit Java Profiler"
   desc "Java CPU and memory profiler"
   homepage "https://www.yourkit.com/features/"
